996a1ac0268b2fde9965d20fd228b4d372716fc1
buriato <bair.ungo@yandex.com>
2018-12-19 17:58:09 +0100

    finished section statistics 15

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
+  mode: 'development',
   output: {
     filename: 'bundle.js'
   },
